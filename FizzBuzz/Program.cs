d6cbbcf4a9097361179bcc45b6b709e5a6967bb4
Nelson <nelson.jf.silva@gmail.com>
2013-06-01 23:05:32 +0100

    Classic FizzBuzz
    Testing VS 2012 Integration with github

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isDiv5 = false;
+            bool isDiv3 = false;
+            bool isBoth = false;
+
+            for (int i = 0; i < 99; i++)
+            {
+                isDiv5 = i % 5 == 0;
+                isDiv3 = i % 3 == 0;
+                isBoth = isDiv5 && isDiv3;
+
+                if (isBoth)
+                    Console.WriteLine("FizzBuzz");
+                else if (isDiv3)
+                    Console.WriteLine("Fizz");
+                else if (isDiv5)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
